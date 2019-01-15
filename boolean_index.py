fc97856c6aee244cad808fb4377fac5240ade91e
Benoit Matet <benoit@macbook-pro-de-benoit-1.home>
2019-01-15 19:50:49 +0100

    added boolean query handling via operator tree

--- boolean_index.py
+++ boolean_index.py
@@ -3,7 +3,8 @@ from typing import List
 import numpy as np
 from scipy.sparse import lil_matrix, csc_matrix
 from index import Index
-
+from tree_operator import Operator
+from tree_operator import draw
 
 class BooleanIndex(Index):
     def build_cacm(self, filename: str):
@@ -19,7 +20,7 @@ class BooleanIndex(Index):
 
         nb_terms = 5462
         nb_docs = 3204
-        self.incidence_matrix = lil_matrix((nb_terms, nb_docs))
+        self.incidence_matrix = lil_matrix((nb_terms, nb_docs), dtype=bool)  # Boolean index
 
         for line in raw_file:
             if line.startswith("."):
@@ -49,49 +50,70 @@ class BooleanIndex(Index):
                             term_id += 1
                             self.terms_to_id[term] = term_id
                             self.id_to_term[term_id] = term
-                        self.incidence_matrix[term_id, doc_id] = 1  # Boolean index
+                        self.incidence_matrix[self.terms_to_id[term], doc_id] = True  # Boolean index
 
         # Add last doc
         self.doc_to_id[str(doc_id + 1) + ' -' + title] = doc_id
         self.id_to_doc[doc_id] = str(doc_id + 1) + ' -' + title
 
-        self.incidence_matrix = self.incidence_matrix.tocsc()  # Faster column slicing
+        self.incidence_matrix = self.incidence_matrix.tocsr()  # Faster row slicing
 
-    def treat_query(self, query: str) -> np.array:
-        print(query)
-        query_words = query.split()
-        bool_operations = []
-        bool_terms = []
-        for word in query_words:
+
+    def create_bool_tree(self, query: list, left_op:Operator, node_id) -> Operator:  # returns a tree of operations, given by its root
+        print("Function called : ", query, left_op, node_id)
+        node_id += 1
+        my_node_id = node_id
+        if len(query) > 0:
+            word = query.pop(0)
             if word in ["AND", "NOT", 'OR']:
-                bool_operations.append(word)
+                if (word == "AND" or word == "OR") and left_op is None:
+                    raise ValueError("Query was ill formulated. Please specify a term before an operation \"AND\" or \"OR\"")
+                tree_dic[my_node_id] = Operator(keyword=word, left=left_op, right=self.create_bool_tree(query, None, node_id))                
+
             else:
-                term = self.normalize(word)
-                # TODO : check if term != ''
-                bool_terms.append(self.terms_to_id[term])  # Get the corresponding id
-        bool_result = self.compute_bool_result(bool_operations, bool_terms)
-
-        for idx_doc in bool_result.nonzero()[1]:
-            print(self.id_to_doc[idx_doc])
-        return bool_result.nonzero()
-
-    def compute_bool_result(self, operations: List[str], bool_terms: List[int]) -> lil_matrix:
-        result = self.incidence_matrix[bool_terms[0], :]
-        for i in range(len(operations)):
-            if operations[i] == 'NOT':
-                term = np.absolute(1 - self.incidence_matrix[bool_terms[i + 1], :].toarray())
-                result = result.multiply(csc_matrix(term))
-            elif operations[i] == 'AND':
-                result = result.multiply(self.incidence_matrix[bool_terms[i + 1], :])
-            elif operations[i] == 'OR':
-                result = result.maximum(self.incidence_matrix[bool_terms[i + 1], :])
-        return result
-
-
+                if left_op is not None:
+                    raise ValueError("Query was ill formulated : Two words without an operator in-between")
+                term = self.normalize(word)[0]
+                if term != "":
+                    token_id = self.terms_to_id[term]  # Get the corresponding id
+                    tree_dic[my_node_id] = self.create_bool_tree(query, left_op=Operator(token_id=token_id), node_id=node_id)
+                else:
+                    raise ValueError("empty word detected") 
+            print("{}. Word : {} - Op : {}".format(my_node_id, word, tree_dic[my_node_id]))
+            return tree_dic[my_node_id]
+        else:
+            print("Reached end of query")
+            return left_op       
+            
+    def treat_query(self, query: str) -> Operator:
+        print(query)
+        query_words = query.split(" ")
+        print(query_words)
+        return self.create_bool_tree(query_words, None, 0)
+          
+    def compute_bool_result(self, op):  # recursive function to compute the result of a tree given its root
+        if hasattr(op, 'token_id'):
+            return self.incidence_matrix[op.token_id]
+        else:
+            if op.keyword == "OR":
+                return self.compute_bool_result(op.left) + self.compute_bool_result(op.right)
+            if op.keyword == "AND":
+                return self.compute_bool_result(op.left).multiply(self.compute_bool_result(op.right))
+            if op.keyword == "NOT":
+                return 1 - self.compute_bool_result(op.right)                
+        
+        
 if __name__ == '__main__':
+    
     PATH_TO_DATA = 'data'
-
+    print("Creating index...")
     index = BooleanIndex()
     index.build_cacm(os.path.join(PATH_TO_DATA, 'CACM', 'cacm.all'))
-
-    # index.treat_query('Assistant OR program')
+    tree_dic = {}
+    tree = index.treat_query('Assistant OR program')
+    print("word 'program' found in the following docs :")
+    print(index.incidence_matrix[14])
+    print("____________________________________________")
+    print(index.compute_bool_result(tree))
+    
+    
