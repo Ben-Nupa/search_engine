1a4beed70d5f70af91b9e3ca897df956a5695ac7
SylvestreSakti <sylvestre.prabakaran@gmail.com>
2019-01-21 17:54:19 +0100

    Boolean index for cs276 and Q1 to 5

--- boolean_index.py
+++ boolean_index.py
@@ -58,6 +58,60 @@ class BooleanIndex(Index):
 
         self.incidence_matrix = self.incidence_matrix.tocsr()  # Faster row slicing
 
+    def build_cs276(self, directory_name: str):
+        """
+        Uses the CS276 (Stanford) collection to build boolean_index and saves
+
+        Args :
+            directory_name : String containing the path to the CS276 dataset (pa1-data)
+        """
+        doc_id = -1
+        term_id = -1
+
+        nb_terms = 353975
+        nb_docs = 98998
+
+        for block_id in range(10):
+            list_of_files = os.listdir(os.path.join(directory_name, str(block_id)))
+            block_inc_matrix = lil_matrix((nb_terms, nb_docs))
+            for file_id in range(len(list_of_files)):
+                if file_id % 100 == 0:
+                    print("Completed {} % of block {}...".format(int(100 * file_id / len(list_of_files)), block_id))
+                # Reading the document
+                file = open(os.path.join(directory_name, str(block_id), list_of_files[file_id]), "r")
+                content = file.readlines()
+                file.close()
+                # Adding the document to both doc_to_id and id_to_doc dictionaries
+                doc_id += 1
+                doc = os.path.join(str(block_id), list_of_files[file_id])
+                self.doc_to_id[doc] = doc_id
+                self.id_to_doc[doc_id] = doc
+                # Counting the terms
+                for line in content:
+                    terms = self.normalize(line)
+                    for term in terms:
+                        if term != "":
+                            if term not in self.terms_to_id.keys():
+                                term_id += 1
+                                self.terms_to_id[term] = term_id
+                                self.id_to_term[term_id] = term
+                            block_inc_matrix[
+                                self.terms_to_id[term], doc_id] = 1  # +=1 if we want to count the frequency of the term
+            print("Saving block " + str(block_id))
+            block_inc_matrix = block_inc_matrix.tocsc()
+            save_npz("block_inc_matrix" + str(block_id) + ".npz", block_inc_matrix)
+
+    def load_cs276_index(self):
+        """
+        Load the CS276 saved index in .npz format (the build_cs276 function should have been run before
+        in order to have the .npz files)
+        """
+        nb_terms = 353975
+        nb_docs = 98998
+        self.incidence_matrix = lil_matrix((nb_terms, nb_docs))
+        for block_id in range(10):
+            self.incidence_matrix += np.load("block_inc_matrix" + str(block_id) + ".npz")
+        self.incidence_matrix = self.incidence_matrix.tocsc()  # Faster column slicing
 
     def create_bool_tree(self, query: list, left_op:Operator, node_id) -> Operator:  # returns a tree of operations, given by its root
         print("Function called : ", query, left_op, node_id)
